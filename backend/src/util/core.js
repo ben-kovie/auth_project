a7f04af19e8474344e50752ab8a575a39c1a0ece
ben-kovie <benedict.yekovie@gmail.com>
2026-03-11 14:51:52 +0100

    update core

--- backend/src/util/core.js
+++ backend/src/util/core.js
@@ -1,7 +1,7 @@
 import cors from "cors"
 
 const corsOptions = {
-  origin: "https://authproject-omega.vercel.app/", // Vite dev server
+  origin: "https://authproject-omega.vercel.app", // Vite dev server
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
